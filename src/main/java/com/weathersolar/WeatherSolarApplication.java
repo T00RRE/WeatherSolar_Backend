80a288cedb60143ebf5928fc5116a0b246e502b4
Mikolaj Leski <mikolajleski5@gmail.com>
2025-06-21 23:47:57 +0200

    Now its Done

--- src/main/java/com/weathersolar/WeatherSolarApplication.java
+++ src/main/java/com/weathersolar/WeatherSolarApplication.java
@@ -2,11 +2,14 @@ package com.weathersolar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.function.client.WebClient;
+import com.weathersolar.config.SolarProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(SolarProperties.class)
 @ComponentScan(basePackages = {"com.weathersolar", "com.weathersolar.config"})
 public class WeatherSolarApplication {
 
